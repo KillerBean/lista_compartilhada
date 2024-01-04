c71a63641379dd9c1514bde20dfb0f66d6b6d888
Luis Roseno <nando.roseno@gmail.com>
2024-01-04 19:29:49 -0300

    feat: aprimoramento da pagina settings

--- lib/src/pages/settings/settings_page.dart
+++ lib/src/pages/settings/settings_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:lista_compartilhada/src/pages/settings/settings_store.dart';
@@ -11,11 +13,37 @@ class SettingsPageWidget extends StatelessWidget {
 
     return ListenableBuilder(
       listenable: settingsStore,
-      builder: (context, child) => Switch(
-        value: settingsStore.themeMode == ThemeMode.dark,
-        onChanged: (value) {
-          settingsStore.themeMode = value ? ThemeMode.dark : ThemeMode.light;
-        },
+      builder: (context, child) => ListView(
+        children: [
+          const ListTile(
+            // dense: true,
+            contentPadding: EdgeInsets.only(top: 8),
+            title: Card(
+              shape: BeveledRectangleBorder(),
+              elevation: 2,
+              child: Center(
+                child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Settings',
+                      style: TextStyle(fontSize: 24),
+                    )),
+              ),
+            ),
+          ),
+          SwitchListTile(
+            dense: true,
+            title: const Text(
+              'Dark Mode',
+              style: TextStyle(fontSize: 18),
+            ),
+            value: settingsStore.themeMode == ThemeMode.dark,
+            onChanged: (value) {
+              settingsStore.themeMode =
+                  value ? ThemeMode.dark : ThemeMode.light;
+            },
+          ),
+        ],
       ),
     );
   }
