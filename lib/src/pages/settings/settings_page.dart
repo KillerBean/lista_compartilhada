27a60ac10abb87eab089be9bb140707a4a9904dd
Luis Roseno <nando.roseno@gmail.com>
2024-01-04 19:31:52 -0300

    fix: remove import não utilizado

--- lib/src/pages/settings/settings_page.dart
+++ lib/src/pages/settings/settings_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:lista_compartilhada/src/pages/settings/settings_store.dart';
